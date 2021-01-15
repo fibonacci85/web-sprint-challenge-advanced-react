f84e3d6533479ce5cc3893913fa5843d6797c266
Francis Bonacic <francisbonacic@icloud.com>
2021-01-15 10:26:48 -0800

    finished but having trouble with one of my test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    //arrange
+    render(<CheckoutForm />);
+    //act
+    const header = screen.getByText(/checkout form/i)
+    //assert
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const fName =screen.getByLabelText(/first name/i)
+    userEvent.type(fName, "Francis");
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const success = screen.getByTestId(/successMessage/i);
+    expect(success).toBeInTheDocument();
+});
--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,19 +1,35 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-const localStorage = (key, initialValue) => {
+// const useLocalStorage = (key, initialValue) => {
 
-    const [storedValue, setStoredValue] = useState(() => {
-        const item = window.localStorage.getItem(key)
-        return item ? JSON.parse(item) : initialValue
-    })
+//     // const [storedValue, setStoredValue] = useState(() => {
+//     //     const item = window.localStorage.getItem(key)
+//     //     return item ? JSON.parse(item) : initialValue
+//     // })
 
-    const setValue = (value) => {
-        setStoredValue(value)
-        window.localStorage.setItem(key, JSON.stringify(value))
-    }
+//     // const setValue = (value) => {
+//     //     setStoredValue(value)
+//     //     window.localStorage.setItem(key, JSON.stringify(value))
+//     // }
 
-    return [storedValue, setValue]
+//     // return [storedValue, setValue]
 
-}
+//     const [storedValue, setStoredValue] = useState(()=>{
+//         if (localStorage.getItem(key)) {
+//             return JSON.parse(localStorage.getItem(key));
+//         }
 
-export default localStorage;
+//         localStorage.setItem(key, JSON.stringify(initialValue));
+//         return initialValue;
+//     });
+
+//     const setValue = value => {
+//         setStoredValue(value);
+//         localStorage.setItem(key, JSON.stringify(value));
+//     }
+
+//     return [storedValue, setValue];
+// }
+
+
+// export default useLocalStorage;
